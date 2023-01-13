0a907d440449eed20185cfdb1ce8a611ef5ba1b4
Andy Fillebrown <andy@docEdub.com>
2023-01-13 15:50:28 -0500

    Add test.py script I've been using to test .sofa file read/write

--- utils/test.py
+++ utils/test.py
@@ -1,5 +1,36 @@
 import hrtf
 
-cipic = hrtf.CipicHRTF('../data/subject_003.sofa', 44100.0)
-print(cipic.azimuths.shape)
-print(cipic.elevations.shape)
+cipic = hrtf.CipicHRTF('data/template.sofa', 44100.0)
+# print(cipic.azimuths.shape)
+# print(cipic.elevations.shape)
+
+impulses = cipic.impulses
+print(impulses.shape)
+elevations = cipic.elevations
+azimuths = cipic.azimuths
+
+hrtf.create_cipic_hrtf('data/template.sofa', 'data/predict.sofa', impulses, elevations, azimuths)
+
+# import json
+
+# impulsesFlattened = impulses.ravel()
+# impulseArray = []
+# for i in range(len(impulsesFlattened)):
+#     impulseArray.append(impulsesFlattened[i])
+
+# sourcePositionArray = []
+# for i in range(len(elevations)):
+#     sourcePositionArray.append(elevations[i])
+#     sourcePositionArray.append(azimuths[i])
+#     sourcePositionArray.append(1)
+
+# jsonData = {}
+
+# with open("data/template.sofa.json", "r") as jsonFile:
+#     jsonData = json.load(jsonFile)
+
+# jsonData["Variables"]["Data_IR"]["Values"] = impulseArray
+# jsonData["Variables"]["SourcePosition"]["Values"] = sourcePositionArray
+
+# with open("data/template-2.sofa.json", "w") as jsonFile:
+#     jsonFile.write(json.dumps(jsonData, indent = 4))
